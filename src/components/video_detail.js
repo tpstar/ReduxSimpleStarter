a7fb80f50d068f3791a33342a7c3eacbcd4661c4
tpstar <hanlee12@gmail.com>
2017-05-23 23:14:05 -0400

    lesson 31

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -6,11 +6,10 @@ const VideoDetail = ({video}) => {
   }
 
   const videoId = video.id.videoId;
-  const url = `https://www.youtube.com/embed/${videoId}`;
-  console.log(videoId);
+  const url = `https://www.youtube.com/embed/${videoId}`
   return (
     <div className="video-detail col-md-8">
-      <div className="embed-reponsive embed-reponsive-16by9">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
@@ -18,7 +17,9 @@ const VideoDetail = ({video}) => {
         <div>{video.snippet.description}</div>
       </div>
     </div>
-  );
+  )
 };
+
+
 
 export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyAIYVlHfuUDCL7yJ2jRU_ztJPd4uQN2dto';
 
 
-
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'DanTDM'}, (videos) => {
-      this.setState({ videos });
-    });
-
+    this.videoSearch('surfboards')
   }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     );
   }
